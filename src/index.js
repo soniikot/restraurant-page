ed9285f7646a937ae6edd530675a9ec294480e9c
soniikot <soniikot@gmail.com>
2024-04-07 17:49:04 -0700

    add menuItem class

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ console.log('Hello');
 import './style.css';
 
 import { createPage } from "./home.js";
-import { menu} from "./menu.js";
+import { createMenuPage} from "./menu.js";
 import { contact } from "./contact.js";
 
 document.getElementById("homeButton").addEventListener("click", createPage);
 window.addEventListener("load", createPage);
 
-document.getElementById("menuButton").addEventListener("click", menu);
+document.getElementById("menuButton").addEventListener("click", createMenuPage);
 
 document.getElementById("contactButton").addEventListener("click", contact);
---
+++ src/menu.js
@@ -0,0 +1,38 @@
+const menuArray = [];
+const content = document.querySelector('.content');
+
+class MenuItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  
+  info() {
+    return `${this.title} ${this.price}`;
+  }
+}
+
+const truffle = new MenuItem('Truffle ham and eggs sandwich', 14);
+const salmonSandwich = new MenuItem('Norwegian salmon sandwich', 14);
+const croissant = new MenuItem('Croissant with bacon, egg, porcini mushroom sauce and parmesan shavings', 15);
+const salmonQuiche = new MenuItem('Salmon and spinach quiche', 20);
+const veggies = new MenuItem('Grilled vegetables with buffalo mozzarella served with extra virgin olive oil and basil', 25);
+const lasagna = new MenuItem('Florian Lasagna with meat ragù and béchamel sauce', 26);
+
+function addMenuItem(...menuItems) {
+  menuArray.push(...menuItems);
+}
+
+addMenuItem(truffle, salmonSandwich, croissant, salmonQuiche, veggies, lasagna);
+
+const menuCard = document.createElement("div");
+menuCard.classList.add("menuCard");
+
+for (let i = 0; i < menuArray.length; i++) {
+  const menuItem = menuArray[i];
+  const menuItemElement = document.createElement("div");
+  menuItemElement.textContent = menuItem.info();
+  menuCard.appendChild(menuItemElement);
+}
+
+content.appendChild(menuCard);
