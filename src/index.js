87e5bea55104f2581a6ae0e14acd54f1ce28c8e4
soniikot <soniikot@gmail.com>
2024-04-23 15:30:56 -0700

    fix loading issue

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ import { createMenuPage} from "./menu.js";
 import { createContactPage } from "./contact.js";
 
 document.getElementById("homeButton").addEventListener("click", createPage);
-<<<<<<< HEAD
 window.addEventListener('DOMContentLoaded',createPage);
-=======
-window.addEventListener('load',createPage);
->>>>>>> 7ea8a62e5f2029dbfe2d3feeeac70edc7df6620a
 
 document.getElementById("menuButton").addEventListener("click", createMenuPage);
 
